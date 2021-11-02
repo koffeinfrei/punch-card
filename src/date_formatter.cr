02178a04645d431ae9488a7dbc2a8a9bf9eef0c3
Alexis Reigel <mail@koffeinfrei.org>
2021-11-02 19:53:17 +0100

    use local date for output

--- src/date_formatter.cr
+++ src/date_formatter.cr
@@ -7,18 +7,18 @@ class DateFormatter
   end
 
   def day_short
-    date.to_s(I18n::Format.get("day_short"))
+    date.to_local.to_s(I18n::Format.get("day_short"))
   end
 
   def day_with_name
-    date.to_s(I18n::Format.get("day_with_name"))
+    date.to_local.to_s(I18n::Format.get("day_with_name"))
   end
 
   def month_with_name
-    date.to_s(I18n::Format.get("month_with_name"))
+    date.to_local.to_s(I18n::Format.get("month_with_name"))
   end
 
   def time
-    date.to_s(I18n::Format.get("time"))
+    date.to_local.to_s(I18n::Format.get("time"))
   end
 end
