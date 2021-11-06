f2a2ac8dd0679e25777303111646c99a91aee0f8
Alexis Reigel <mail@koffeinfrei.org>
2021-11-06 19:13:02 +0100

    single input parser, no sub commands anymore
    this allows for simpler inputs, without the necessity of always
    providing a subcommand

--- src/punch-card.cr
+++ src/punch-card.cr
@@ -9,12 +9,137 @@ require "./day_table_output"
 require "./month_table_output"
 require "./date_formatter"
 
-class PunchCard < Cli::Supercommand
+abstract class InputAction
+  getter input
+
+  def initialize(@input : String)
+  end
+
+  abstract def matches?
+  abstract def run
+end
+
+class StartInputAction < InputAction
+  def matches?
+    input.starts_with?("start ")
+  end
+
+  def run
+    time = input.split(" ").last
+    Store.new.insert(EntryType::Start, DateParser.parse(time))
+  end
+end
+
+class StopInputAction < InputAction
+  def matches?
+    input.starts_with?("stop ")
+  end
+
+  def run
+    time = input.split(" ").last
+    Store.new.insert(EntryType::Stop, DateParser.parse(time))
+  end
+end
+
+class SpanInputAction < InputAction
+  def matches?
+    input.includes?("-")
+  end
+
+  def run
+    start, stop = input.split("-")
+
+    store = Store.new
+    store.insert(EntryType::Start, DateParser.parse(start))
+    store.insert(EntryType::Stop, DateParser.parse(stop))
+  end
+end
+
+class ShowDayInputAction < InputAction
+  def matches?
+    input == "today"
+  end
+
+  def run
+    date = DateParser.parse(input)
+
+    raw_entries = Store.new.select(date)
+    day_summary_entry = DaySummary.new(date, raw_entries).get
+
+    puts DayTableOutput.new(day_summary_entry).render
+  end
+end
+
+class ShowMonthInputAction < InputAction
+  def matches?
+    input == "month"
+  end
+
+  def run
+    month = Time.local.month
+    year = Time.local.year
+
+    day_summary_entries = (1..Time.local.day).to_a
+      .compact_map { |day|
+        date = Time.local(year, month, day)
+        date unless date.saturday? || date.sunday?
+      }
+      .map do |date|
+        raw_entries = Store.new.select(date)
+        DaySummary.new(date, raw_entries).get
+      end
+
+    puts MonthTableOutput.new(day_summary_entries).render
+  end
+end
+
+class InputActionParser
+  getter input
+
+  def initialize(@input : String)
+  end
+
+  def parse
+    if StartInputAction.new(input).matches?
+      StartInputAction.new(input)
+    elsif StopInputAction.new(input).matches?
+      StopInputAction.new(input)
+    elsif SpanInputAction.new(input).matches?
+      SpanInputAction.new(input)
+    elsif ShowDayInputAction.new(input).matches?
+      ShowDayInputAction.new(input)
+    elsif ShowMonthInputAction.new(input).matches?
+      ShowMonthInputAction.new(input)
+    end
+  end
+end
+
+class PunchCard < Cli::Command
   HELP_FOOTER = "Made with ☕️  by Koffeinfrei"
 
   version {{ `shards version #{__DIR__}`.chomp.stringify }}
 
   class Options
+    arg "input",
+      required: true,
+      desc: <<-DESC
+        The input can be any string adhering to any of the following definitions. The format implies a specific action.
+
+        Add an entry:
+          start <time>
+            The time you start working. Can be 'now' or a specific time like '08:15'
+
+          stop <time>
+            The time you stop working. Can be 'now' or a specific time like '17:30'
+
+          span <start time>-<stop time>
+            A time span of work. Must be in the format '<from>-<to>'. <from> and <to> can be 'now' or a specific time like '17:30'
+
+        Show an entry:
+          <date>
+            Can be 'today', 'yesterday', 'month' or a specific date like '#{DateFormatter.new(Time.local).day_short}'
+        DESC
+
     help
     version
   end
@@ -24,101 +149,10 @@ class PunchCard < Cli::Supercommand
     footer HELP_FOOTER
   end
 
-  class Start < Cli::Command
-    class Options
-      arg "start",
-        required: true,
-        desc: "The time you start working. Can be 'now' or a specific time like '08:15'"
-      help
-    end
-
-    class Help
-      header "Adds a start time entry"
-      footer HELP_FOOTER
-    end
-
-    def run
-      Store.new.insert(EntryType::Start, DateParser.parse(args.start))
-    end
-  end
-
-  class Stop < Cli::Command
-    class Options
-      arg "stop",
-        required: true,
-        desc: "The time you stop working. Can be 'now' or a specific time like '17:30'"
-      help
-    end
-
-    class Help
-      header "Adds a stop time entry"
-      footer HELP_FOOTER
-    end
-
-    def run
-      Store.new.insert(EntryType::Stop, DateParser.parse(args.stop))
-    end
-  end
-
-  class Span < Cli::Command
-    class Options
-      arg "span",
-        required: true,
-        desc: "Enter a time span. Must be in the format '<from>-<to>'. <from> and <to> can be 'now' or a specific time like '17:30'"
-      help
-    end
-
-    class Help
-      header "Adds a span (start-stop) time entry"
-      footer HELP_FOOTER
-    end
-
-    def run
-      start, stop = args.span.split("-")
-
-      store = Store.new
-      store.insert(EntryType::Start, DateParser.parse(start))
-      store.insert(EntryType::Stop, DateParser.parse(stop))
-    end
-  end
-
-  class Show < Cli::Command
-    class Options
-      arg "date",
-        required: true,
-        desc: "The date for which to show a summary. Can be 'today', 'yesterday', 'month' or a specific date like '#{DateFormatter.new(Time.local).day_short}'"
-      help
-    end
-
-    class Help
-      header "Outputs a summary of your tracked time"
-      footer HELP_FOOTER
-    end
-
-    def run
-      if args.date == "month"
-        month = Time.local.month
-        year = Time.local.year
-        day_summary_entries = (1..Time.local.day).to_a
-          .compact_map { |day|
-            date = Time.local(year, month, day)
-            date unless date.saturday? || date.sunday?
-          }
-          .map do |date|
-            raw_entries = Store.new.select(date)
-            DaySummary.new(date, raw_entries).get
-          end
-
-        puts MonthTableOutput.new(day_summary_entries).render
-      else
-        date = DateParser.parse(args.date)
-
-        raw_entries = Store.new.select(date)
-        day_summary_entry = DaySummary.new(date, raw_entries).get
-
-        puts DayTableOutput.new(day_summary_entry).render
-      end
-    end
+  def run
+    input = ([args.input] + args.nameless_args).join(" ")
+    action = InputActionParser.new(input).parse
+    action.run unless action.nil?
   end
 end
 
