2ffb376088cc165495eb6348b55dfa916cc00c36
Alexis Reigel <mail@koffeinfrei.org>
2022-02-27 13:12:08 +0100

    use record macro for model class

--- src/store/entry.cr
+++ src/store/entry.cr
@@ -1,10 +1,6 @@
 require "./entry_type"
 
-class Store
-  struct Entry
-    property type, time, project
-
-    def initialize(@type : EntryType, @time : Time, @project : String | Nil)
-    end
-  end
-end
+record Store::Entry,
+  type : EntryType,
+  time : Time,
+  project : String | Nil
