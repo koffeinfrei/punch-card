abcffb76406d11877dd139ecfe9a3db71ea6d7fa
Alexis Reigel <mail@koffeinfrei.org>
2022-09-01 20:13:42 +0200

    convert rounded up 60 minutes to 1 hour
    instead of getting 4:60 we should output 5:00

--- spec/number_formatter_spec.cr
+++ spec/number_formatter_spec.cr
@@ -7,6 +7,10 @@ describe NumberFormatter do
     it "formats a positive number" do
       NumberFormatter.new(4.08).as_time.should eq "4:05"
     end
+
+    it "converts rounded up 60 minutes to 1 hour" do
+      NumberFormatter.new(4.999).as_time.should eq "5:00"
+    end
   end
 
   describe ".as_prefixed_time" do
--- src/number_formatter.cr
+++ src/number_formatter.cr
@@ -9,7 +9,13 @@ class NumberFormatter
     hours = abs_number.to_i
     minutes = abs_number - hours
 
-    "#{hours}:#{(60 * minutes).round.to_i.to_s.rjust(2, '0')}"
+    rounded_minutes = (60 * minutes).round.to_i
+    if rounded_minutes == 60
+      hours += 1
+      rounded_minutes = 0
+    end
+
+    "#{hours}:#{rounded_minutes.to_s.rjust(2, '0')}"
   end
 
   def as_prefixed_time
